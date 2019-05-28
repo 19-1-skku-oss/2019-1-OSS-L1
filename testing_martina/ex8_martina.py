c72a94e0d5f72cd3ca253c89deb6c46b85364c53
Martina <martinalee0528@gmail.com>
2019-05-28 03:19:52 -0700

    ex8_martina.py edited
    Signed-off-by: Martina <martinalee0528@gmail.com>

--- testing_martina/ex8_martina.py
+++ testing_martina/ex8_martina.py
@@ -1,5 +1,10 @@
-for number in range(1500,2700):
-    if number%5==0 or number%7==0:
-        print(number)
-    else: continue
-    
+import pytest
+
+def func():
+	for number in range(1500,2700):
+	    if number%5==0 or number%7==0:
+		print(number)
+	    else: continue
+
+def test_func():
+	assert func() 
