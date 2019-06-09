5848b4aeba24975343b5ccccddf75be4a5086a79
hyungjc <48250000+hyungjc@users.noreply.github.com>
2019-06-09 13:15:44 +0900

    Delete conftest.py

--- doc/pytest_doc_code/Chapter4/conftest.py
+++
@@ -1,5 +0,0 @@
-from test_foocompare import Foo
-
-def pytest_assertrepr_compare(op, left, right):
-	if isinstance(left, Foo) and isinstance(right, Foo) and op == "==":
-		return ["Comparing Foo instances:", " vals: %s != %s" % (left.val, right.val)]
