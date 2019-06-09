efdcd37fe0bae578acf75faf56fdea6e226e841b
hyungjc <48250000+hyungjc@users.noreply.github.com>
2019-06-09 13:14:49 +0900

    Delete conftest.py

--- doc/pytest_doc_code/Chapter15/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="class")
-def db_class(request):
-	class DummyDB(object):
-		pass
-	# set a class attribute on the invoking test context
-	request.cls.db = DummyDB()
